f86805ec0570ff576874da1b6cf8cfabe4eb3fb3
Chris <cr1123@gmail.com>
2020-10-14 12:37:10 -0400

    Mouse Mover

---
+++ MouseSS/Exec.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MouseSS
+{
+    public class Exec : Form
+    {
+        public Exec() { }
+
+        public void Start()
+        {
+            Console.WriteLine("How many minutes to wait?");
+            string minutesToWait = Console.ReadLine();
+            Console.WriteLine("How many hours to run?");
+            string hoursToRun = Console.ReadLine();
+
+            int minutes;
+            if (int.TryParse(minutesToWait, out minutes))
+            {
+                if ((minutes * 60 * 1000) < int.MaxValue)
+                {
+                    int millisecondsToWait = minutes * 60 * 1000;
+                    double hours;
+                    if (double.TryParse(hoursToRun, out hours))
+                    {
+                        long current = DateTime.Now.Ticks;
+                        long stop = DateTime.Now.AddHours(hours).Ticks;
+                        while (current < stop)
+                        {
+                            Thread.Sleep(millisecondsToWait);
+                            MoveCursorUp();
+                            Thread.Sleep(millisecondsToWait);
+                            MoveCursorDown();
+                            current = DateTime.Now.Ticks;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("exiting...");
+        }
+
+        /// <summary>
+        /// Move Cursor Up
+        /// <see cref="https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor.position?redirectedfrom=MSDN&view=netcore-3.1#System_Windows_Forms_Cursor_Position"/>
+        /// </summary>
+        private void MoveCursorUp()
+        {
+            Cursor = new Cursor(Cursor.Current.Handle);
+            Point currentposition = Cursor.Position;
+            Cursor.Position = new Point(currentposition.X - 1, currentposition.Y - 1);
+        }
+
+        /// <summary>
+        /// Move Cursor Down
+        /// <see cref="https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.cursor.position?redirectedfrom=MSDN&view=netcore-3.1#System_Windows_Forms_Cursor_Position"/>
+        /// </summary>
+        private void MoveCursorDown()
+        {
+            Cursor = new Cursor(Cursor.Current.Handle);
+            Point currentposition = Cursor.Position;
+            Cursor.Position = new Point(currentposition.X + 1, currentposition.Y + 1);            
+        }
+    }
+}
---
+++ MouseSS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exec e = new Exec();
+            e.Start();
+        }
+
+    }
+}
